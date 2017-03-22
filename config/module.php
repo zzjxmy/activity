294263afd6083e597141210d8287373bcde629c9
zhangzhijian <137512638@qq.com>
2017-03-22 14:50:51 +0800

    项目立项

---
+++ config/module.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangzhijian
+ * Signature: change self
+ * Date: 2017/3/14
+ * Time: 14:51
+ */
+
+return [
+    //点赞
+    'like' => [
+        'is_open' => true,
+        'callback' => ''
+    ],
+    //投票
+    'vote' => [
+        'is_open' => true,
+    ],
+    //活动页打赏
+    'activity_play_tour' => [
+        'is_open' => true,
+    ],
+    //短信
+    'sms' => [
+        'is_open' => true,
+    ],
+    //红包
+    'red_packet' => [
+        'is_open' => true,
+    ]
+];
