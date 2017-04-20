a5400f456b79fba3ce44c6a298f6013a230157a7
guochao <guochao@yupaopao.cn>
2017-04-20 18:42:34 +0800

    合并33

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+*/
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+
+Route::get('logout', 'Auth\LoginController@logout');
+
+Route::group(['namespace' => 'Admin','middleware' => ['auth']],function(){
+    Route::resource('activity','ActivityController');
+    Route::get('/', 'IndexController@index');
+
+    Route::resource('permission','PermissionController');//权限列表
+    Route::resource('role','RoleController');//角色列表
+    Route::resource('user','UserController');//用户列表
+});
